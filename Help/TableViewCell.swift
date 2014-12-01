bc1330df89ad841f56331b3b5707130760b9bc79
Adarshkumar Pavani <apavani@andrew.cmu.edu>
2014-11-30 20:01:36 -0500

    Testing changes

--- Help/TableViewCell.swift
+++ Help/TableViewCell.swift
@@ -21,7 +21,7 @@ class TableViewCell: UITableViewCell {
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        // Configure the view for the selected state...
     }
 
 }
